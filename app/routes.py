8b16d4c9bb10ee3da5a905a5ede8aa6032c23deb
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 14:06:56 -0500

    uuid

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 class Details(db.Model):
     # id: uuid.UUID = db.Column(postgresql.UUID(as_uuid=True), unique=True,
     #                                    nullable=False, server_default=sqlalchemy.text('uuid_generate_v4()'))
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.UUID, primary_key=True)
     animal = db.Column(db.String)
 
 def create_tables():
@@ -27,7 +27,7 @@ def create_tables():
 @app.route('/index')
 def index():
     print('HELLO!')
-    q = Details(id=str(uuid.uuid4()), animal='cat')
+    q = Details(id=uuid.uuid4(), animal='cat')
     db.session.add(q)
     db.session.commit()
 
