0c977ea20b96acd57765fa0c5bf70d42673d948c
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 12:26:46 -0500

    3 different files

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,12 @@ import datetime
 from app import app
 from flask_sqlalchemy import SQLAlchemy 
 
+import os 
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 db = SQLAlchemy()
 
 @app.route('/')
@@ -11,4 +17,5 @@ def index():
 
     db.session.add(datetime.datetime.now().time())
     db.session.commit()
+
     return "Nusaiba was here :)"
