4d68fdc6c78d247bb56b63c05d6653cc320cbcb0
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 12:45:01 -0500

    3 different files

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lvopweizdnegil:b14d73a558cb9f531af9ba1af25549d0c15b128cd0756933c2886fb5080444a7@ec2-54-198-213-75.compute-1.amazonaws.com:5432/dd58a1j890ha1q'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/index')
 def index():
-
+    print('HELLO!')
     db.session.add(datetime.datetime.now().time())
     db.session.commit()
 
