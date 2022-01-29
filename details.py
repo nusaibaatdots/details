ab3e45b4f526d98680fa04bb9cb6feca841ea20d
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 11:54:59 -0500

    3 different files

--- details.py
+++ details.py
@@ -1,15 +1,1 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-
-
-# app = Flask(__name__)
-
-# # Connect to database here, database = db
-
-# @app.route("/")
-# def print_hello():
-#     # add current time to database here
-#     return "<p>Nusaiba was here :)</p>"
-
 from app import app
