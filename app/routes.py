c2ab5d0364f8e08c2bdf8a5e9f27feb48f7063d0
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 12:18:11 -0500

    3 different files

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+import datetime
+
 from app import app
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
 
 @app.route('/')
 @app.route('/index')
 def index():
+
+    db.session.add(datetime.datetime.now().time())
+    db.session.commit()
     return "Nusaiba was here :)"
