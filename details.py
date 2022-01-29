85472a32680e3a87aba228d9e8fbd5eee2bb8027
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 11:23:38 -0500

    3 different files

--- details.py
+++ details.py
@@ -1,14 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvopweizdnegil:b14d73a558cb9f531af9ba1af25549d0c15b128cd0756933c2886fb5080444a7@ec2-54-198-213-75.compute-1.amazonaws.com:5432/dd58a1j890ha1q'
+# app = Flask(__name__)
 
+# # Connect to database here, database = db
 
-#postgresql://[username]:[password]@[host name]/[db name]
+# @app.route("/")
+# def print_hello():
+#     # add current time to database here
+#     return "<p>Nusaiba was here :)</p>"
 
-@app.route("/")
-def print_hello():
-    return "<p>Nusaiba was here :)</p>"
+from app import app
