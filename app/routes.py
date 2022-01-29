d246830392242ba3c5e2ae6f5c0d94cd8971d6e6
Nusaiba Rahman <nusaiba@senddots.com>
2022-01-29 17:22:22 -0500

    with working form

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, render_template
 from datetime import datetime
 
 from app import app
@@ -7,33 +8,59 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid
 
 import os 
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lvopweizdnegil:b14d73a558cb9f531af9ba1af25549d0c15b128cd0756933c2886fb5080444a7@ec2-54-198-213-75.compute-1.amazonaws.com:5432/dd58a1j890ha1q'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 class Details(db.Model):
-    # id: uuid.UUID = db.Column(postgresql.UUID(as_uuid=True), unique=True,
-    #                                    nullable=False, server_default=sqlalchemy.text('uuid_generate_v4()'))
     id = db.Column(db.String, primary_key=True)
     animal = db.Column(db.String)
+
 
 def create_tables():
     db.create_all()
 
 @app.route('/')
-@app.route('/index')
+def homepage_passcode_form():
+    return render_template('homepage_passcode_form.html')
+
+
+@app.route('/', methods=["POST"])
 def index():
-    print('HELLO!')
-    detailsUUID = str(uuid.uuid4().fields[-1])[:10]
-    detailsUUID = str(detailsUUID)
-    q = Details(id=detailsUUID, animal='cat')
+    passcodeInput = request.form["passcodeInput"]
+    nameInput = request.form["nameInput"]
 
-    db.session.add(q)
-    db.session.commit()
+    if passcodeInput == 'taco':
 
-    returnString = "Thanks for visiting. Your UUID is: " + detailsUUID
+        detailsUUID = str(uuid.uuid4().fields[-1])[:10]
+        detailsUUID = str(detailsUUID)
+        q = Details(id=detailsUUID, animal=nameInput)
 
-    return returnString
+        db.session.add(q)
+        db.session.commit()
+
+        returnString = "You're in, " + str(nameInput) + "." + "Your UUID is: " + detailsUUID
+
+        return returnString
+    return 'Not the right passcode'
+
+    # if request.method == 'GET':
+    #     return 'hi!'
+    # if request.method == 'POST':
+    #     form_data = request.form
+    #     return 'got form data'
+ 
+    # passcodeInput = request.form['passcode']
+    # if passcodeInput == 'taco':
+
+    #     detailsUUID = str(uuid.uuid4().fields[-1])[:10]
+    #     detailsUUID = str(detailsUUID)
+    #     q = Details(id=detailsUUID, animal='cat')
+
+    #     db.session.add(q)
+    #     db.session.commit()
+
+    #     returnString = "Thanks for visiting. Your UUID is: " + detailsUUID
+
+    #     return returnString
+    # return 'Not the right passcode'
